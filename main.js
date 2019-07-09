1fbcd9f3416a2dc41064e306bbc3e54b5482b42b
HamidNE <hamidne@mail.ru>
2019-07-09 06:10:03 +0430

    get archive files from directory

--- main.js
+++ main.js
@@ -1,12 +1,28 @@
-const NodeID3 = require('node-id3');
+const fs = require("fs");
+const NodeID3 = require("node-id3");
 
-let tags = NodeID3.read('./music.mp3');
+const tempFolder = "./Temp";
+const directories = ["Pic", "File"];
 
-console.log(tags);
+fs.readdir("./", function(err, files) {
+  if (err) throw err;
 
-tags.copyright = 'Gerama.ir';
-tags.comment.text = 'Gerama.ir';
-tags.image.description = 'Gerama.ir';
+  files
+    .filter(function(file) {
+      return file.match(/\.(zip|rar)$/g);
+    })
+    .forEach(function(file) {
+      directories.forEach(dir => {
+        const path = "./" + file + "/" + dir;
+        if (!fs.existsSync(path)) fs.mkdirSync(path);
+      });
 
+      let tags = NodeID3.read("./music.mp3");
 
-NodeID3.update(tags, './music.mp3');
+      tags.copyright = "Gerama.ir";
+      tags.comment.text = "Gerama.ir";
+      tags.image.description = "Gerama.ir";
+
+      NodeID3.update(tags, "./music.mp3");
+    });
+});

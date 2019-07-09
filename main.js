7495fa2bb753289772b2e01276b568e454bf5506
HamidNE <HamidNE@mail.ru>
2019-07-09 22:53:28 +0430

    edit for music tags

--- main.js
+++ main.js
@@ -2,23 +2,33 @@ const fs = require("fs");
 const NodeID3 = require("node-id3");
 const { execSync } = require("child_process");
 
-fs.readdir("./", function(err, files) {
+fs.readdir("./", function (err, files) {
   if (err) throw err;
 
   files
-    .filter(function(file) {
+    .filter(function (file) {
       return file.match(/\.(zip|rar)$/g);
     })
-    .forEach(function(file) {
+    .forEach(function (file) {
       execSync(`mkdir musics`);
       execSync(`rar e ${file} musics`);
 
-      let tags = NodeID3.read("./music.mp3");
+      fs.readdir("./musics", function (err, musics) {
+        if (err) throw err;
 
-      tags.copyright = "Gerama.ir";
-      tags.comment.text = "Gerama.ir";
-      tags.image.description = "Gerama.ir";
+        musics
+          .filter(function (music) {
+            return music.match(/\.mp3$/g);
+          })
+          .forEach(function (music) {
+            let tags = NodeID3.read(`./music/${music}`);
 
-      NodeID3.update(tags, "./music.mp3");
+            tags.copyright = "Gerama.ir";
+            tags.comment.text = "Gerama.ir";
+            tags.image.description = "Gerama.ir";
+
+            NodeID3.update(tags, `./music/${music}`);
+          });
+      });
     });
 });
